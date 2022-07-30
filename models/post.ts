f5a05567247de8b7f206da639081431bc8e75246
Kulklex <kulklex@gmail.com>
2022-07-30 12:41:53 +0100

    Posts Users Categories mdels completed

---
+++ models/post.ts
@@ -0,0 +1,30 @@
+import mongoose from "mongoose";
+
+
+
+const PostSchema = new mongoose.Schema({
+    title: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    desc: {
+        type: String,
+        required: true
+    },
+    photo: {
+        type: String,
+        required: true
+    },
+    username: {
+        type: String,
+        required: true
+    },
+    categories: {
+        type: [String],
+        required: false
+    },
+}, {timestamps: true})
+
+export const Post = mongoose.model('Post', PostSchema)
+
---
+++ models/user.ts
@@ -0,0 +1,27 @@
+import mongoose from "mongoose";
+
+
+
+const UserSchema = new mongoose.Schema({
+    username : {
+        type: String,
+        required: true,
+        unique: true //making sure noone can create a user with an existing username
+    },
+    email : {
+        type: String,
+        required: true,
+        unique: true 
+    },
+    password : {
+        type: String,
+        required: true
+    },
+    profilePic: {
+        type: String,
+        default: ""
+    }
+}, {timestamps: true})
+
+export const User = mongoose.model('User', UserSchema)
+
